f16f07b5695b1f7f0a09d7b137e4f98b0048b8b6
nardsqq <nardparagas@gmail.com>
2020-04-01 16:16:22 +0800

    Updates eslint config for interoperability

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'eslint-config-prettier'
+    'prettier'
   ],
   settings: {
     react: { version: '16.13.1' }
